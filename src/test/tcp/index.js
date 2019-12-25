768e5ce5fc58479aed4d024ba2b795996b34458f
appletxm <appletxm@163.com>
2019-12-25 18:41:29 +0800

    update tcp

--- src/test/tcp/index.js
+++ src/test/tcp/index.js
@@ -1,30 +1,30 @@
 const net = require('net')
 const host = '192.168.69.161'
 const server = net.createServer((socket) => {
-    socket.on('data', (data) => {
-        console.log(data.toString())
-        socket.write('hey client!')
-    })
+  socket.on('data', (data) => {
+    console.log(data.toString())
+    socket.write('hey client!')
+  })
 
-    socket.on('end', () => {
-        console.log('client leave')
-    })
+  socket.on('end', () => {
+    console.log('client leave')
+  })
 })
 
-server.listen(8000, () => {
-    console.log('server bound')
+server.listen(8000, host, () => {
+  console.log('server bound')
 })
 
 server.on('connection', () => {
-    console.log('tcp server connection')
+  console.log('tcp server connection')
 })
 
 server.on('close', () => {
-    console.log('tcp server close')
+  console.log('tcp server close')
 })
 
 server.on('error', () => {
-    console.log('tcp server error')
+  console.log('tcp server error')
 })
 
 // setTimeout(() => {
